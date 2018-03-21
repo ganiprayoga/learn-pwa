187ac9e76657e8cb19e409f4e297042d6662b721
Unknown <gani.prayoga@gmail.com>
2018-03-21 10:18:56 +0700

    Cleanup for Section 5

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,15 +1,18 @@
+
 var deferredPrompt;
 
 if (!window.Promise) {
   window.Promise = Promise;
 }
 
-if ('serviceWorker' in navigator)
-{
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(function () {
       console.log('Service worker registered!');
+    })
+    .catch(function (err) {
+      console.log(err);
     });
 }
 
@@ -19,53 +22,3 @@ window.addEventListener('beforeinstallprompt', function (event) {
   deferredPrompt = event;
   return false;
 });
-
-var promise = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    // resolve('This is executed once the timer is done!');
-    reject({code: 500, message: 'Terjadi kesalahan'});
-    // console.log('This is executed once the timer is done!');
-  }, 3000);
-});
-
-fetch('https://httpbin.org/ip')
-  .then(function (response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
-
-fetch('https://httpbin.org/post', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  },
-  mode: 'cors',
-  body: JSON.stringify({message: "Hope this is work!"})
-})
-  .then(function (response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
-
-promise.then(function (text) {
-  return text;
-}).then(function (newText) {
-  console.log(newText);
-}).catch(function (err) {
-  console.log((err.code, err.message));
-});
-
-console.log('This is executed after setTimeout()');
