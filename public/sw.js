23baecdc092955a428e4334118cd3287fa87087e
Unknown <gani.prayoga@gmail.com>
2018-03-27 01:30:55 +0700

    By Sendiri

--- public/sw.js
+++ public/sw.js
@@ -44,7 +44,7 @@ self.addEventListener('fetch', function (event) {
                   return res;
                 })
                 .catch(function (err) {
-                   
+
                 })
             });
         }
